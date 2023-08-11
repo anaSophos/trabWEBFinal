522dabb3a08b5782928ab6208e96711b663899ea
ana410.costa <ana410.costa@UNIFESSPA.LOCAL>
2023-08-11 12:05:39 -0300

    verificação de confirmar senha é igual a senha

--- perfilADM/JS/validacaoCriarConta.js
+++ perfilADM/JS/validacaoCriarConta.js
@@ -38,3 +38,28 @@ function myFunction(e){
     const form = document.querySelector('.form')
     alert("Conta criada com sucesso!")
 }
+
+function verificarSenhas() {
+    const senha = document.querySelector('input[name="senha"]').value;
+    const confirmarSenha = document.querySelector('input[name="confirmarSenha"]').value;
+
+    if (senha === confirmarSenha) {
+        // Senhas correspondem, permitir avançar para outra tela
+        return true;
+    } else {
+        // Senhas não correspondem, exibir mensagem de erro e impedir avanço
+        alert('As senhas não correspondem. Por favor, verifique e tente novamente.');
+        return false;
+    }
+    // Quando o documento estiver carregado, adicionar o evento de clique para o link "Criar Conta"
+    document.addEventListener('DOMContentLoaded', function() {
+        const criarContaLink = document.querySelector('.criar_Conta');
+
+        criarContaLink.addEventListener('click', function(event) {
+            if (!verificarSenhas()) {
+                // Impedir o avanço se as senhas não correspondem
+                event.preventDefault();
+            }
+        });
+    });
+}
